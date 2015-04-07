a9f70a1de9ab8febee061a8710afb0ffabfeaa9b
Phil <sittipol_tribunyatkul@hotmail.com>
2015-04-07 02:09:39 -0700

    fixed some error (these files are still broken though(

--- src/com/group2/eece411/A4/TestDHT.java
+++ src/com/group2/eece411/A4/TestDHT.java
@@ -5,7 +5,7 @@ import com.group2.eece411.DHT;
 public class TestDHT {
 
 	public static void main(String[] args) {
-		DHT dht = new DHT(true, null, 0, 0);
+		DHT dht = new DHT(true, null, 0, 0, null);
 
 		// this node will sit at
 		// "pl1.cs.montana.edu" port 7775
